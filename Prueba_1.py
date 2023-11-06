fe88d06d780a29e8d388a81f02407b507e17ba5e
Victor-B-Reyes <victor_blanco_reyes@outlook.com>
2023-11-05 20:26:57 -0600

    PLN Prueba1

---
+++ Prueba_1.py
@@ -0,0 +1,15 @@
+from urllib import request
+import nltk
+url = "https://www.gutenberg.org/cache/epub/44173/pg44173.txt"
+response = request.urlopen(url)
+raw = response.read().decode('utf8')
+tokens = nltk.word_tokenize(raw)
+text = nltk.Text(tokens)
+
+#text.concordance("albedrío") #Busqueda de palabras y correlacion de oraciones
+#text.similar("realidad") # busqueda cerca de la palabra
+#text.collocations()
+#fdist1 = nltk.FreqDist(text)
+#fdist1.hapaxes()[:50]
+#text.dispersion_plot(["albedrío", "realidad", "libertad"])
+text.generate()
